ad9bd3370586d59ce83ad150db8bd3535cd113e9
Denis <dsivtsov@yahoo.com>
2023-05-12 22:49:42 +0500

    Alfa (Only Demandig Components)

--- TestRedBoon/Assets/Game/GameEngine/Inventory/Scripts/Item/InventoryItemList.cs
+++ TestRedBoon/Assets/Game/GameEngine/Inventory/Scripts/Item/InventoryItemList.cs
@@ -12,20 +12,6 @@ namespace GameEngine.Inventory
         [SerializeField]
         private List<InventoryItemSO> _itemsList;
 
-        //public InventoryItemConfig FindItem(string name)
-        //{
-        //    for (int i = 0, count = this.items.Length; i < count; i++)
-        //    {
-        //        var item = this.items[i];
-        //        if (item.ItemName == name)
-        //        {
-        //            return item;
-        //        }
-        //    }
-
-        //    throw new Exception($"Item {name} is not found!");
-        //}
-
         public List<InventoryItemSO> ListInventoryItemSO => _itemsList;
 
         public override string ToString() => _itemsList.Count.ToString();
